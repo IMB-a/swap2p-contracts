aa5ea4e22f8608ee7b7e24c73b4fc3058f882d0f
Alex Lazarev <lazarev989@gmail.com>
2022-04-27 21:17:16 +0300

    feat: erc-721 to erc-20 escrow

---
+++ contracts/Escrow721To20.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Escrow721To20 is AccessControl {
+    struct Escrow {
+        address xOwner;
+        address xTokenContractAddr;
+        uint256 xIndex;
+        address yOwner;
+        address yTokenContractAddr;
+        uint256 yAmount;
+        bool closed;
+    }
+
+    Escrow[] public escrowList;
+    uint256 public fee;
+
+    mapping(address => uint256[]) public ownerTokens;
+
+    event EscrowCreated(
+        address xOwner,
+        address xTokenContractAddr,
+        uint256 xIndex,
+        address yOwner,
+        address yTokenContractAddr,
+        uint256 yAmount,
+        uint256 escrowIndex
+    );
+
+    event EscrowAccepted(uint256 escrowIndex, address yOwner);
+    event EscrowCanceled(uint256 escrowIndex);
+
+    constructor(uint256 _fee) {
+        fee = _fee;
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function createEscrow(
+        address _xTokenContractAddr,
+        uint256 _xIndex,
+        address _yTokenContractAddr,
+        uint256 _yAmount,
+        address _yOwner
+    ) external payable {
+        require(msg.value >= fee, "not enough fee");
+        require(_xTokenContractAddr != address(0), "x zero address");
+        require(_yTokenContractAddr != address(0), "y zero address");
+        require(_yAmount > 0, "y zero amount");
+
+        IERC721 xToken = IERC721(_xTokenContractAddr);
+        require(
+            xToken.ownerOf(_xIndex) == msg.sender,
+            "you don't have a token"
+        );
+
+        escrowList.push(
+            Escrow({
+                xOwner: msg.sender,
+                xTokenContractAddr: _xTokenContractAddr,
+                xIndex: _xIndex,
+                yOwner: _yOwner,
+                yTokenContractAddr: _yTokenContractAddr,
+                yAmount: _yAmount,
+                closed: false
+            })
+        );
+        uint256 escrowIndex = escrowList.length - 1;
+
+        xToken.transferFrom(msg.sender, address(this), _xIndex);
+
+        emit EscrowCreated(
+            msg.sender,
+            _xTokenContractAddr,
+            _xIndex,
+            _yOwner,
+            _yTokenContractAddr,
+            _yAmount,
+            escrowIndex
+        );
+    }
+
+    function cancelEscrow(uint256 _escrowIndex) external {
+        require(
+            escrowList[_escrowIndex].xOwner == msg.sender,
+            "you'r isn't escrow owner"
+        );
+        require(
+            escrowList[_escrowIndex].closed == false,
+            "escrow already closed"
+        );
+
+        escrowList[_escrowIndex].closed = true;
+        emit EscrowCanceled(_escrowIndex);
+    }
+
+    function acceptEscrow(uint256 _escrowIndex) external {
+        IERC721 xToken = IERC721(escrowList[_escrowIndex].xTokenContractAddr);
+        IERC20 yToken = IERC20(escrowList[_escrowIndex].yTokenContractAddr);
+        require(escrowList[_escrowIndex].closed == false, "escrow closed");
+        require(
+            escrowList[_escrowIndex].yOwner == address(0) ||
+                escrowList[_escrowIndex].yOwner == msg.sender,
+            "escrow not for you"
+        );
+        require(
+            yToken.balanceOf(msg.sender) > escrowList[_escrowIndex].yAmount,
+            "not enought yToken"
+        );
+
+        escrowList[_escrowIndex].closed = true;
+
+        xToken.transferFrom(
+            address(this),
+            msg.sender,
+            escrowList[_escrowIndex].xIndex
+        );
+        yToken.transferFrom(
+            msg.sender,
+            address(this),
+            escrowList[_escrowIndex].yAmount
+        );
+        yToken.transfer(
+            escrowList[_escrowIndex].xOwner,
+            escrowList[_escrowIndex].yAmount
+        );
+
+        emit EscrowAccepted(_escrowIndex, msg.sender);
+    }
+
+    function setFee(uint256 _newFee) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        fee = _newFee;
+    }
+
+    function claimFee(address _to) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        payable(_to).transfer(address(this).balance);
+    }
+
+    function getEscrow(uint256 _escrowIndex)
+        public
+        view
+        returns (Escrow memory)
+    {
+        require(escrowList.length > 0, "no escrows");
+        require(
+            _escrowIndex < escrowList.length,
+            "Id must be < escrows length"
+        );
+
+        return escrowList[_escrowIndex];
+    }
+}
